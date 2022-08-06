4519934bdd3243e679e61c705f03924f39a00b4f
enmanuel93 <fcruzm@ji.gob.do>
2022-08-06 01:17:08 -0400

    add new rateservice to the application layer

---
+++ GNB.InternationalBussinessMen/GNB.WebService/GNB.Application/application.services/RateService.cs
@@ -0,0 +1,21 @@
+﻿using GNB.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GNB.Application.application.services
+{
+    public class RateService
+    {
+        private readonly IRateRepository _rateRepository;
+
+        public RateService(IRateRepository rateRepository)
+        {
+            this._rateRepository = rateRepository;
+        }
+
+
+    }
+}
